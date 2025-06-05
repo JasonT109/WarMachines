428a3c2c025d905c9ecb9ad663d69d817a2aa52b
jtassell <jason.tassell@gmail.com>
2025-06-06 11:38:08 +1200

    For prosperity

---
+++ Assets/Scripts/Props/PropFlipper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropFlipper : MonoBehaviour {
+
+    public Animator FlipperAnimator;
+
+    void ToggleFlipper()
+    {
+        FlipperAnimator.SetTrigger("start");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+            ToggleFlipper();
+    }
+}
